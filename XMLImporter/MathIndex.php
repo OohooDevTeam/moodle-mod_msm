1ec64a4a8c8572afcc27714c5cc03476fa97ef2c
gayoung <gayoung1@ualberta.ca>
2012-06-21 08:57:32 -0600

    -fixed all the problems with dupliecate data in index tables

--- XMLImporter/MathIndex.php
+++ XMLImporter/MathIndex.php
@@ -107,24 +107,97 @@ class MathIndex extends Element
 
         if (!empty($this->symbol))
         {
-            $data->symbol = $this->symbol;
-            $data->symbol_type = $this->symbol_type;
+             $data->symbol_type = $this->symbol_type;
             $data->sortstring = $this->sortstring;
+            $data->symbol = $this->symbol;
+            
+             $numOfRecords = $DB->count_records('msm_index_symbol');
+            if ($numOfRecords > 0)
+            {
+                // need the limit to be $numOfRecords+1 to process the last record
+                for ($i = 1; $i < $numOfRecords + 1; $i++)
+                {
+                    $string = $DB->get_field('msm_index_symbol', 'symbol', array('id' => $i));
 
-            $this->id = $DB->insert_record($this->symboltable, $data);
+                    if ($string == $this->symbol)
+                    {
+                        $recordID = $i;
+                        break;
+                    }
+                    else
+                    {
+                        $recordID = false;
+                    }
+                }
+            }
+            else
+            {
+                $recordID = false;
+            }
+
+            if (empty($recordID))
+            {
+                  $this->id = $DB->insert_record($this->symboltable, $data);
+            }         
         }
 
         if (!empty($this->names))
         {
             foreach ($this->names as $key => $name)
             {
-//                $recordID = $this->checkForRecord($this, 'name');
-//
-//                if (empty($recordID))
-//                {
-//                    print_object($name);
-                $name->saveIntoDb($name->position, 'index');
-//                }
+                $firststring = $name->name["first"];
+                $laststring = $name->name["last"];
+                $middlestring = $name->name["middle"];
+
+                $numOfRecords = $DB->count_records('msm_person');
+                if ($numOfRecords > 0)
+                {
+                    // need the limit to be $numOfRecords+1 to process the last record
+                    for ($i = 1; $i < $numOfRecords + 1; $i++)
+                    {
+                        $first = $DB->get_field('msm_person', 'firstname', array('id' => $i));
+                        $last = $DB->get_field('msm_person', 'lastname', array('id' => $i));
+                        $middle = $DB->get_field('msm_person', 'middlename', array('id' => $i));
+
+                        if ((!empty($first)) && ($first == $firststring))
+                        {
+                            if ((!empty($last)) && ($last == $laststring))
+                            {
+                                if ((!empty($middle)) && ($middle == $middlestring))
+                                {
+                                    $recordID = $i;
+                                    break;
+                                }
+                                else if ((empty($middle)) && (empty($middlestring))) //record exists where first and last name match but middle is empty
+                                {
+                                    $recordID = $i;
+                                    break;
+                                }
+                                else
+                                {
+                                    $recordID = false;
+                                }
+                            }
+                            else //first name matched but not lastname
+                            {
+                                $recordID = false;
+                            }
+                        }
+                        else // first name did not match
+                        {
+                            $recordID = false;
+                        }
+                    }
+                }
+                else // the number of record in the table is less or equal to zero
+                {
+                    $recordID = false;
+                }
+
+                if (empty($recordID))
+                {
+                    $name->saveIntoDb($name->position, 'index');
+                }
             }
         }
 
@@ -137,7 +210,7 @@ class MathIndex extends Element
             if ($numOfRecords > 0)
             {
                 // need the limit to be $numOfRecords+1 to process the last record
-                for ($i = 1; $i < $numOfRecords+1; $i++)
+                for ($i = 1; $i < $numOfRecords + 1; $i++)
                 {
                     $string = $DB->get_field('msm_index_glossary', 'term', array('id' => $i));
 
@@ -161,18 +234,17 @@ class MathIndex extends Element
 
             if (empty($recordID))
             {
-                echo "insert record";
                 $this->id = $DB->insert_record($this->glossarytable, $data);
             }
         }
 
         foreach ($this->infos as $info)
         {
-           $numOfRecords = $DB->count_records('msm_info');
+            $numOfRecords = $DB->count_records('msm_info');
             if ($numOfRecords > 0)
             {
                 // need the limit to be $numOfRecords+1 to process the last record
-                for ($i = 1; $i < $numOfRecords+1; $i++)
+                for ($i = 1; $i < $numOfRecords + 1; $i++)
                 {
                     $string = $DB->get_field('msm_info', 'info_content', array('id' => $i));
 
@@ -191,8 +263,8 @@ class MathIndex extends Element
             {
                 $recordID = false;
             }
-            
-            if(empty($recordID))
+
+            if (empty($recordID))
             {
                 $info->saveIntoDb($info->position);
             }
