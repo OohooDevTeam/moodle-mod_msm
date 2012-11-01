3aa410c917d38f5686b1e5539b6d5408302bb603
gayoung <gayoung1@ualberta.ca>
2012-11-01 15:24:46 -0600

    - changed drag and drop from jquery to html 5 - also now can drag to any location within the content div - resize done with jquery - now when drag from left div to middle, has proper form for def/theorem - problem with duplicate id and drop field specification

--- js/authorNav.js
+++ js/authorNav.js
@@ -45,3 +45,124 @@ function closeSetting()
         });
     });
 }
+
+function allowDrop(event)
+{
+    event.preventDefault();
+}
+
+function dragStart(event)
+{    
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain",
+        (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY) + ',' + event.target.id);
+    //    event.dataTransfer.setData("Text", event.target.id);
+    //    console.log(style);
+    event.target.style.cursor = 'move';   
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
+} 
+
+function drop(event)
+{
+    var offset = event.dataTransfer.getData("text/plain").split(',');
+    //    var id = event.dataTransfer.getData("text")
+    var draggedElement = document.getElementById(offset[2]);
+  
+    var stringmatch = draggedElement.id.match(/copied-/);
+    
+    if(stringmatch == null)
+    {
+        var clonedDraggedElement = draggedElement.cloneNode(true);
+        clonedDraggedElement.id = "copied-"+draggedElement.id;
+        clonedDraggedElement.className = "copied-"+draggedElement.className;
+
+        if(clonedDraggedElement.id == 'copied-msm_def')
+        {
+            var defSelectMenu = '<select class="msm_unit_child_dropdown">\n\
+                                <option value="Notation">Notation</option>\n\
+                                <option value="Definition">Definition</option>\n\
+                                <option value="Agreement">Agreement</option>\n\
+                                <option value="Convention">Convention</option>\n\
+                                <option value="Axiom">Axiom</option>\n\
+                                <option value="Terminology">Terminology</option>\n\
+                            </select>';
+            var defTitleField = '<input class="msm_unit_child_title" id="msm_def_title_input" name="msm_def_title" placeholder=" Title of Definition"/>';
+            var defContentField = '<input class="msm_unit_child_content" id="msm_def_content_input" name="msm_def_content" placeholder="Need to add moodle form here?"/>';
+            
+            var defAddChildButton = '<button class= "msm_unit_child_button" id="msm_def_child_add" type="button"> (+) Add Associated Information </button>';
+        
+            while(clonedDraggedElement.firstChild)
+            {
+                clonedDraggedElement.removeChild(clonedDraggedElement.firstChild);
+            }
+        
+            clonedDraggedElement.innerHTML = defSelectMenu + defTitleField + defContentField + defAddChildButton;
+        }
+        else if(clonedDraggedElement.id == 'copied-msm_theorem')
+        {
+            var theoremSelectMenu = '<select class="msm_unit_child_dropdown">\n\
+                                <option value="Theorem">Theorem</option>\n\
+                                <option value="Proposition">Proposition</option>\n\
+                                <option value="Lemma">Lemma</option>\n\
+                                <option value="Corollary">Corollary</option>\n\
+                            </select>';
+            var theoremTitleField = '<input class="msm_unit_child_title" id="msm_theorem_title_input" name="msm_theorem_title" placeholder=" Title of Theorem"/>';
+            var theoremContentField = '<input class="msm_unit_child_content" id="msm_theorem_content_input" name="msm_theorem_content" placeholder=" Need to add moodle form here?"/>';
+            var theoremAddChildButton = '<button class= "msm_unit_child_button" id="msm_theorem_child_add" type="button"> (+) Add Associated Information </button>';
+        
+            while(clonedDraggedElement.firstChild)
+            {
+                clonedDraggedElement.removeChild(clonedDraggedElement.firstChild);
+            }
+        
+            clonedDraggedElement.innerHTML = theoremSelectMenu + theoremTitleField + theoremContentField + theoremAddChildButton;
+        }
+        
+        
+        event.target.appendChild(clonedDraggedElement);
+    }  
+    else
+    {        
+        
+        // correct target met, then move the draggable object to specified coordinates
+        draggedElement.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+        draggedElement.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+        event.target.appendChild(draggedElement);   
+    }
+    
+    event.preventDefault();
+    return false;
+}
+
+function resizeDiv(event)
+{    
+    var id = event.srcElement.id;
+     
+    var idMatch = id.match(/copied-/);
+    var targetElement = document.getElementById(id);
+     
+    if(idMatch != null)
+    {      
+        targetElement.style.borderStyle = "dotted";
+        targetElement.style.borderWidth = "thin";
+        targetElement.className = "copied-msm_structural_element_selected";
+        $("#"+id).resizable({
+            containement: "parent",
+            ghost: true,
+            helper: "resizable-helper",
+            minHeight: 150,
+            minWidth: 320
+        });
+    }
+    
+//    else
+//    {
+//        alert("blur");
+//        targetElement.style.borderStyle = "dashed";
+//        targetElement.style.borderWidth = "2";
+//    }
+}
