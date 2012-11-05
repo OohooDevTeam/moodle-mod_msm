0ab4b6cec74737ef60f65ad16693c9646ac7a276
gayoung <gayoung1@ualberta.ca>
2012-11-05 15:06:05 -0700

    - fixed a bug where there were duplicate id's when element is dragged and dropped to content generation area

--- js/authorNav.js
+++ js/authorNav.js
@@ -46,20 +46,86 @@ function closeSetting()
     });
 }
 
-function processDroppedChild(e, droppedId)
+function processDroppedChild(e, droppedId, _index)
 {
+    var clonedCurrentElement = $("<div></div>");
+    
     if(droppedId == 'msm_def')
     {
-        alert("def");
+        var defSelectMenu = $('<select class="msm_unit_child_dropdown">\n\
+                                <option value="Notation">Notation</option>\n\
+                                <option value="Definition">Definition</option>\n\
+                                <option value="Agreement">Agreement</option>\n\
+                                <option value="Convention">Convention</option>\n\
+                                <option value="Axiom">Axiom</option>\n\
+                                <option value="Terminology">Terminology</option>\n\
+                            </select>');
+        var defTitleField = $('<input class="msm_unit_child_title" id="msm_def_title_input" name="msm_def_title" placeholder=" Title of Definition"/>');
+        var defContentField = $('<input class="msm_unit_child_content" id="msm_def_content_input" name="msm_def_content" placeholder="Need to add moodle form here?"/>');
+            
+        var defAssoMenu = $('<b> Choose an associated information: </b>\n\
+                            <select class="msm_associated_dropdown">\n\
+                                <option value="None">None</option>\n\
+                                <option value="Info">Quick Info</option>\n\
+                                <option value="Comment">Comment</option>\n\
+                                <option value="Explanation">Explanation</option>\n\
+                                <option value="Example">Example</option>\n\
+                                <option value="Illustration">Illustration</option>\n\
+                            </select>');
+        
+        //        var currentElement = document.getElementById(droppedId);
+        clonedCurrentElement.attr("id", "copied_msm_def-"+_index++);
+        clonedCurrentElement.attr("class", "copied_msm_structural_element");
+        clonedCurrentElement.append(defSelectMenu);
+        clonedCurrentElement.append(defTitleField);
+        clonedCurrentElement.append(defContentField);
+        clonedCurrentElement.append(defAssoMenu);
+       
+        clonedCurrentElement.appendTo('#msm_editor_middle_droparea');
     }
     else if(droppedId == 'msm_theorem')
     {
-        alert("theorem");
+        var theoremSelectMenu = $('<select class="msm_unit_child_dropdown">\n\
+                                <option value="Theorem">Theorem</option>\n\
+                                <option value="Proposition">Proposition</option>\n\
+                                <option value="Lemma">Lemma</option>\n\
+                                <option value="Corollary">Corollary</option>\n\
+                            </select>');
+        var theoremTitleField = $('<input class="msm_unit_child_title" id="msm_theorem_title_input" name="msm_theorem_title" placeholder=" Title of Theorem"/>');
+        var theoremContentField = $('<input class="msm_unit_child_content" id="msm_theorem_content_input" name="msm_theorem_content" placeholder=" Need to add moodle form here?"/>');
+        var theoremAssoMenu = $('<b> Choose an associated information: </b>\n\
+                            <select class="msm_associated_dropdown">\n\
+                                <option value="None">None</option>\n\
+                                <option value="Info">Quick Info</option>\n\
+                                <option value="Comment">Comment</option>\n\
+                                <option value="Explanation">Explanation</option>\n\
+                                <option value="Example">Example</option>\n\
+                                <option value="Illustration">Illustration</option>\n\
+                                <option value="Proof">Proof</option>\n\
+                            </select>');
+        
+        clonedCurrentElement.attr("id", "copied_msm_theorem"+_index++);
+        clonedCurrentElement.attr("class", "copied_msm_structural_element");
+        clonedCurrentElement.append(theoremSelectMenu);
+        clonedCurrentElement.append(theoremTitleField);
+        clonedCurrentElement.append(theoremContentField);
+        clonedCurrentElement.append(theoremAssoMenu);
+        
+        clonedCurrentElement.appendTo('#msm_editor_middle_droparea');
+       
     }
     else 
     {
         alert("other child");
     }
+    
+    $('.copied_msm_structural_element').draggable({
+        appendTo: "msm_editor_middle_droparea",
+        containment: "msm_editor_middle_droparea",
+        cursor: "move"
+    });
+    
+    return _index;
 }
 
 //
