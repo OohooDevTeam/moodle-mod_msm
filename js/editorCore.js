057f597c78fa1886f9c9be00bf2c8122931f597b
gayoung <gayoung1@ualberta.ca>
2012-11-20 15:12:32 -0700

    - resizing of image done although need to still diable resizing when user is done - fixed bugs where error occurs when user cancels the transaction in browse window, and if user selects the wrong file - fixed some css issues

--- js/editorCore.js
+++ js/editorCore.js
@@ -204,51 +204,98 @@ function showImagePreview(evt)
     
     _imageIndex++;
     
-    // Only process image files.
-    if (files[0].type.match('image.*')) {
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e) {
-                // Render thumbnail.
-                var div = document.createElement('div');
-                div.className = "msm_img_previews";
-                div.id = "msm_img_preview-"+filepickerId[1]+"-";
-                
-                var image = document.createElement("img");
-                image.id = "msm_img_thumbnail-"+filepickerId[1]+"-"+_imageIndex;
-                image.className = "msm_thumbnails";
-                image.src = e.target.result;
-                image.title = escape(theFile.name);
-                image.onclick = "resizeThumbnail(event)";
-
-                div.appendChild(image);
-                document.getElementById('msm_file_list-'+filepickerId[1]).insertBefore(div, null);
-                
-                //resizing the image so it fits into the div
-                var imageWidth = image.offsetWidth;
-                var divWidth = div.offsetWidth;
-                if(imageWidth > divWidth)
-                {
-                    image.width = divWidth;
-                }
-            };
-        })(files[0]);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(files[0]);
+    var outputElement = document.getElementById('msm_file_list-'+filepickerId[1]);
+    
+    // remove image inserted before --> according to XML schema, only one image allowed
+    if(outputElement.hasChildNodes())
+    {
+        $('#'+outputElement.firstChild.id).empty().remove();
     }
-}
-
-function resizeThumbnail(e)
-{
-    var currentElement = e.target.id;
     
+    // Only process image files
+    if(files.length != 0) // condition to deal with canceled transaction in browse window
+    {
+        if (files[0].type.match('image.*')) {
+            var reader = new FileReader();
+
+            // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    // Render thumbnail.
+                    var imageDiv = document.createElement('div');
+                    imageDiv.className = "msm_img_previews";
+                    imageDiv.id = "msm_img_preview-"+filepickerId[1]+"-"+_imageIndex;
+                
+                    var image = document.createElement("img");
+                    image.id = "msm_img_thumbnail-"+filepickerId[1]+"-"+_imageIndex;
+                    image.className = "msm_thumbnails";
+                    image.src = e.target.result;
+                    image.title = escape(theFile.name);     
+                    image.onclick = function(){
+                        $(this).resizable
+                        ({
+                            ghost: true
+                        });
+                    };
+                    image.onblur = function(){
+                        $(this).resizable({
+                            disabled: true
+                        }); 
+                    };
+
+                    imageDiv.appendChild(image);
+                    document.getElementById('msm_file_list-'+filepickerId[1]).insertBefore(imageDiv, null);
+                
+                    //resizing the image so it fits into the div
+                    var imageWidth = image.offsetWidth;
+                    var divWidth = imageDiv.offsetWidth;
+                    if(imageWidth > divWidth)
+                    {
+                        image.width = divWidth;
+                    }
+                };
+            })(files[0]);
+
+            // Read in the image file as a data URL.
+            reader.readAsDataURL(files[0]);
+        }
+        else
+        {
+            // alert dialog to notify the user to select only image file
+            $("<div class='dialogs' id='msm_wrongFileType'> Please select an image file. </div>").appendTo('#msm_pic_filepicker-'+_index);
+            
+            $( "#msm_wrongFileType" ).dialog({
+                modal: true,
+                buttons: {
+                    Ok: function(event, ui) {
+                        //removing the wrong file from event object
+                        document.getElementById('msm_pic_filepicker-'+_index).value = '';
+                        $( this ).dialog( "close" );
+                    }
+                }
+            });
+        }
+    }
+    
+    // reinitialize tinyMCE 
+    tinyMCE.init({
+        mode:"textareas",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+        width: "96%",
+        height: "70%",
+        theme: "advanced",
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,cleanup,help,code,|,insertdate,inserttime,preview",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,advhr,|,ltr,rtl",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,forecolor,backcolor",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        skin : "o2k7",
+        skin_variant : "silver"
+    });
     
 }
-
-
 
 /**
  * to save contents created in middle editor panel
