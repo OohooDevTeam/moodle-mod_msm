d376f3f3c83196638a7a55389b446d74a51c7dc8
gayoung <gayoung1@ualberta.ca>
2013-01-14 16:29:23 -0700

    - created EditorSubordinate class

---
+++ editorCreation/EditorSubordinate.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EditorSubordinate
+ *
+ * @author User
+ */
+class EditorSubordinate extends EditorElement
+{
+   function __construct()
+    {
+        $this->tablename = 'msm_subordinate';
+    }
+
+    public function getFormData($idNumber, $position)
+    {
+        
+    }
+
+    public function insertData($parentid, $siblingid, $msmid)
+    {
+        
+    }
+}
+
+?>
