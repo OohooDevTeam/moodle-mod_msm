b1183d30dd5c361c7ca24b319f6cca3661ac4958
gayoung <gayoung1@ualberta.ca>
2013-07-25 12:14:12 -0600

    - minor changes to index creating script files

--- XMLImporter/loadGlossary.php
+++ XMLImporter/loadGlossary.php
@@ -22,6 +22,8 @@ require_once('Unit.php');
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_once(dirname(dirname(__FILE__)) . '/lib.php');
 
+global $CFG;
+
 $string = $_POST["moduleinfo"];
 
 $ids = explode(',', $string);
--- XMLImporter/loadSymbol.php
+++ XMLImporter/loadSymbol.php
@@ -23,6 +23,8 @@ require_once('Unit.php');
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_once(dirname(dirname(__FILE__)) . '/lib.php');
 
+global $CFG;
+
 $string = $_POST["moduleinfo"];
 
 $ids = explode(',', $string);
